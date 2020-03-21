58e820b434ae89141294941cd6c31f3915c5d036
Tristan Partin <tristan@partin.io>
2020-03-21 15:27:20 -0500

    Update the legalese

--- coronavirus-client/src/components/legal.component.js
+++ coronavirus-client/src/components/legal.component.js
@@ -6,34 +6,35 @@ export default class Legal extends Component {
 			<div style={{ marginBottom: "50px" }} className="auth-inner-large">
 				<h2>Legal</h2>
 
-				<h4 id="privacy-policy">Privacy Policy</h4>
+				<h3 id="privacy-policy">Privacy Policy</h3>
 
 				<div>
-					<small>Last updated: March 20<sup>th</sup> 2020 </small>
+					<small>Last updated: March 21,2020 </small>
 				</div>
 				<hr />
 
 				<div>
 					<p>
-						My Company (change this) ("us", "we", or "our") operates http://www.mysite.com (change
-						this) (the "Site"). This page informs you of our policies regarding the collection, use
-						and disclosure of Personal Information we receive from users of the Site. We use your
-						Personal Information only for providing and improving the Site. By using the Site, you
-						agree to the collection and use of information in accordance with this policy.
+						MYMathApps ("we", "our") operates https://mymathapps.com (the "Site"). This page informs
+						you of our policies regarding the collection, use and disclosure of Personal Information
+						we receive from users of the Site. We use your Personal Information only for providing
+						access to the content displayed on our site. By using the Site, you agree to the
+						collection and use of information in accordance with this policy.
 					</p>
 				</div>
 
-				<h5>Information Collection And Use</h5>
+				<h4>Information Collection And Use</h4>
 
 				<div>
 					<p>
 						While using our Site, we may ask you to provide us with certain personally identifiable
 						information that can be used to contact or identify you. Personally identifiable
-						information may include, but is not limited to your name ("Personal Information").
+						information includes only your name ("Personal Information"), email address, and
+						password.
 					</p>
 				</div>
 
-				<h5>Log Data</h5>
+				<h4>Log Data</h4>
 
 				<div>
 					<p>
@@ -41,38 +42,37 @@ export default class Legal extends Component {
 						visit our Site ("Log Data"). This Log Data may include information such as your
 						computer's Internet Protocol ("IP") address, browser type, browser version, the pages of
 						our Site that you visit, the time and date of your visit, the time spent on those pages
-						and other statistics. In addition, we may use third party services such as Google
-						Analytics that collect, monitor and analyze this … The Log Data section is for
-						businesses that use analytics or tracking services in websites or apps, like Google
-						Analytics. For the full disclosure section, create your own Privacy Policy.
+						and other statistics. We do not use third party services to track your activity across
+						the Site unlike competitors. None of the Log Data is shared with third parties.
 					</p>
 				</div>
 
-				<h5>Communications</h5>
+				<h4>Communications</h4>
 
 				<div>
 					<p>
 						We may use your Personal Information to contact you with newsletters, marketing or
-						promotional materials and other information that ... The Communications section is for
-						businesses that may contact users via email (email newsletters) or other methods. For
-						the full disclosure section, create your own Privacy Policy.
+						promotional materials and other information that would help you to discover news about
+						the site including but not limited to sales, new products, or product updates. In
+						addition, in the case of a forgotten password, we will email you a temporary password.
 					</p>
 				</div>
 
-				<h5>Cookies</h5>
+				<h4 id="cookies">Cookies</h4>
 
 				<div>
 					<p>
-						Cookies are files with small amount of data, which may include an anonymous unique
+						Cookies are files with small amounts of data, which may include an anonymous unique
 						identifier. Cookies are sent to your browser from a web site and stored on your
 						computer's hard drive. Like many sites, we use "cookies" to collect information. You can
 						instruct your browser to refuse all cookies or to indicate when a cookie is being sent.
 						However, if you do not accept cookies, you may not be able to use some portions of our
-						Site.
+						Site. Our use of cookies is limited to first party cookies, which are strictly used to
+						provide access to our content. We do not use thrid-party cookies to track you in anyway.
 					</p>
 				</div>
 
-				<h5>Security</h5>
+				<h4>Security</h4>
 
 				<div>
 					<p>
@@ -83,25 +83,40 @@ export default class Legal extends Component {
 					</p>
 				</div>
 
-				<h5>Changes To This Privacy Policy</h5>
+				<h4>Changes To This Privacy Policy</h4>
 
 				<div>
 					<p>
-						This Privacy Policy is effective as of (add date) and will remain in effect except with
-						respect to any changes in its provisions in the future, which will be in effect
+						This Privacy Policy is effective as of (March 21, 2020) and will remain in effect except
+						with respect to any changes in its provisions in the future, which will be in effect
 						immediately after being posted on this page. We reserve the right to update or change
-						our Privacy Policy at any time and you should check this Privacy Policy periodically.
+						our Privacy Policy at any time, and you should check this Privacy Policy periodically.
 						Your continued use of the Service after we post any modifications to the Privacy Policy
 						on this page will constitute your acknowledgment of the modifications and your consent
 						to abide and be bound by the modified Privacy Policy. If we make any material changes to
 						this Privacy Policy, we will notify you either through the email address you have
-						provided us, or by placing a prominent notice on our website.
+						provided us, or by placing a prominent notice on our Site.
 					</p>
 				</div>
-				<h5>Contact Us</h5>
+
+				<h3 id="terms-of-service">Terms of Service</h3>
 
 				<div>
-					<p>If you have any questions about this Privacy Policy, please contact <a href="mailto:yasskin@math.tamu.edu">yasskin@math.tamu.edu</a>.</p>
+					<p>
+						The content on our Site is provided "as-is". We are not liable for any grades you may
+						get while using our Site. Access to the Site requires an internet connection. Any
+						proprietary content may not be downloaded or distributed without the express written
+						consent of MYMathApps.
+					</p>
+				</div>
+
+				<h3 id="contact-us">Contact Us</h3>
+
+				<div>
+					<p>
+						If you have any questions about this Privacy Policy, please contact{" "}
+						<a href="mailto:yasskin@math.tamu.edu">yasskin@math.tamu.edu</a>.
+					</p>
 				</div>
 			</div>
 		);
