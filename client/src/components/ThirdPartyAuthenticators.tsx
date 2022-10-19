94b8df49fdb07bc2d89634c4cf9801103a739892
b06611033 <messiharden@gmail.com>
2022-10-19 15:22:41 -0500

    Google login function

--- client/src/components/ThirdPartyAuthenticators.tsx
+++ client/src/components/ThirdPartyAuthenticators.tsx
@@ -1,12 +1,54 @@
-import React from "react";
+//import React from "react";
 import { Button } from "semantic-ui-react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { gapi } from "gapi-script";
+import React, { useState, useEffect } from "react";
 
 type ThirdPartyAuthenticatorsProps = {
 	action: "login" | "sign-up";
 };
 
+const clientId = "887279930655-lb7apr295ikt6fua81mg50p8il4tue1k.apps.googleusercontent.com";
+
+//useEffect(() => {
+	const initClient = () => {
+		  gapi.client.init({
+		  clientId: clientId,
+		  scope: ''
+		});
+	 };
+	 gapi.load('client:auth2', initClient);
+ //});
+
 const ThirdPartyAuthenticators: React.FC<ThirdPartyAuthenticatorsProps> = (props): JSX.Element => {
+	const onSuccess = (res: any) => {
+		console.log("success:", res);
+	};
+	const onFailure = (err: any) => {
+		console.log("failed:", err);
+	};
 	return (
+		<GoogleLogin
+			clientId={clientId}
+			render={(renderProps) => (
+				<Button
+					onClick={renderProps.onClick}
+					disabled={renderProps.disabled}
+					color="red"
+					content="Google"
+					fluid
+					icon="google"
+					size="medium"
+				/>
+			)}
+			buttonText="Sign in with Google"
+			onSuccess={onSuccess}
+			onFailure={onFailure}
+			cookiePolicy={"single_host_origin"}
+			isSignedIn={true}
+		/>
+	);
+	/*return (
 		<Button
 			color="red"
 			content="Google"
@@ -19,7 +61,7 @@ const ThirdPartyAuthenticators: React.FC<ThirdPartyAuthenticatorsProps> = (props
 			}
 			size="medium"
 		/>
-	);
+	);*/
 };
 
 export default ThirdPartyAuthenticators;
