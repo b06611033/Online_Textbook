a8d0a21bf75b82dea3acb02909e292a17073ea09
kaichh <huangkc3@gmail.com>
2022-12-05 13:07:06 -0600

    delete some testing code used in development

--- client/src/components/ThirdPartyAuthenticators.tsx
+++ client/src/components/ThirdPartyAuthenticators.tsx
@@ -10,54 +10,9 @@ type ThirdPartyAuthenticatorsProps = {
   action: "login" | "sign-up";
 };
 
-// const ThirdPartyAuthenticators: React.FC<ThirdPartyAuthenticatorsProps> = (
-//   props
-// ): JSX.Element => {
-
-//   const handleLogin = async () => {
-//     const response = await axios
-//       .get(`${process.env.REACT_APP_SERVER_DOMAIN}/auth/test`)
-//       .catch(function (error) {
-//         if (error.response) {
-//           // The request was made and the server responded with a status code
-//           // that falls out of the range of 2xx
-//           console.log(error.response.data);
-//           console.log(error.response.status);
-//           console.log(error.response.headers);
-//         } else if (error.request) {
-//           // The request was made but no response was received
-//           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-//           // http.ClientRequest in node.js
-//           console.log(error.request);
-//         } else {
-//           // Something happened in setting up the request that triggered an Error
-//           console.log("Error", error.message);
-//         }
-//       });
-//     console.log(response);
-
-//     // localStorage.setItem("user", JSON.stringify(response.data));
-//     // window.alert("登入成功。您現在將被重新導向到個人資料頁面。");
-//     // setCurrentUser(AuthService.getCurrentUser());
-//     // nagivate("/profile");
-//   };
-
-//   return (
-//     <Button
-//       color="red"
-//       content="Google"
-//       fluid
-//       icon="google"
-//       onClick={handleLogin}
-//       size="medium"
-//     />
-//   );
-// };
-
 const ThirdPartyAuthenticators: React.FC<ThirdPartyAuthenticatorsProps> = (
   props
 ): JSX.Element => {
-  const [user, setUser] = useState<User | undefined>();
   const history = useHistory();
   const ctx = useContext(ApplicationContext);
   const handleLogin = () => {
@@ -68,8 +23,7 @@ const ThirdPartyAuthenticators: React.FC<ThirdPartyAuthenticatorsProps> = (
     );
 
     const listener = window.addEventListener("message", (message) => {
-      console.log(message.data);
-      console.log(message.data.user.googleID);
+      //console.log(message.data);
       localStorage.setItem("token", JSON.stringify(message.data.token));
 
       const currentUser = {
