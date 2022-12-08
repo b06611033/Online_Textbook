d3a9fd3ebe40115ee6be0072fbe6993a5a6a0781
b06611033 <messiharden@gmail.com>
2022-12-08 16:35:31 -0600

    render textbook pages on heroku

--- client/src/components/Textbook.tsx
+++ client/src/components/Textbook.tsx
@@ -7,36 +7,37 @@ import { Product } from "../entities";
 import ProductHeader from "./ProductHeader";
 
 type TextbookProps = {
-	child: React.ReactNode;
-	codeName: string;
-	image: string;
+  child: React.ReactNode;
+  codeName: string;
+  image: string;
 };
 
 const Textbook: React.FC<TextbookProps> = (props): JSX.Element => {
-	const { data } = useSWR<Product, Error>(`/api/products/${props.codeName}`, (url) =>
-		fetch(url, { method: "GET" }).then((res) => res.json())
-	);
+  //   const { data } = useSWR<Product, Error>(
+  //     `/api/products/${props.codeName}`,
+  //     (url) => fetch(url, { method: "GET" }).then((res) => res.json())
+  //   );
 
-	if (!data) {
-		return <Loader active inline="centered" />;
-	}
+  //   if (!data) {
+  //     return <Loader active inline="centered" />;
+  //   }
 
-	return (
-		<>
-			<ProductHeader hasSample image={props.image} product={data} />
-			<Divider></Divider>
-			{props.child}
-			<p>
-				<img
-					alt="The National Science Foundation"
-					src="https://www.mymathapps.com/images/nsf.png"
-					style={{ verticalAlign: "middle", height: "3em" }}
-				/>
-				&emsp;Supported in part by NSF DUE CCLI / TUES Grants 0737209 / 1123170 (Meade) and 0737209
-				/ 1123255 (Yasskin)
-			</p>
-		</>
-	);
+  return (
+    <>
+      <ProductHeader hasSample image={props.image} />
+      <Divider></Divider>
+      {props.child}
+      <p>
+        <img
+          alt="The National Science Foundation"
+          src="https://www.mymathapps.com/images/nsf.png"
+          style={{ verticalAlign: "middle", height: "3em" }}
+        />
+        &emsp;Supported in part by NSF DUE CCLI / TUES Grants 0737209 / 1123170
+        (Meade) and 0737209 / 1123255 (Yasskin)
+      </p>
+    </>
+  );
 };
 
 export default Textbook;
