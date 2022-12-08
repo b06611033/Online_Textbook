a2cc9479d270c2ad599bc5ef13b704e529f7d3a2
b06611033 <messiharden@gmail.com>
2022-12-08 15:14:32 -0600

    test

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: yarn run build-client && node index.js
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(
   session({
